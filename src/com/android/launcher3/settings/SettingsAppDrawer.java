Bootleggers-BrokenLab/packages_apps_Launcher3-old

Launcher3: Allow to tweak blur radius on all apps and on overview
Change-Id: I724cf8eeab40c915009b36f030b0a79788f749e2

src/com/android/launcher3/settings/SettingsAppDrawer.java
@@ -37,6 +37,8 @@ import androidx.preference.PreferenceGroup.PreferencePositionCallback;
 import androidx.preference.PreferenceScreen;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.internal.util.bootleggers.BootlegUtils;
+
 import com.android.launcher3.LauncherAppState;
 import com.android.launcher3.LauncherFiles;
 import com.android.launcher3.R;
@@ -172,6 +174,9 @@ public class SettingsAppDrawer extends FragmentActivity
                         }
                     });
                     return true;
+                case Utilities.KEY_ALLOW_ALL_APPS_BLUR:
+                case Utilities.KEY_ALL_APPS_BLUR:
+                    return BootlegUtils.supportsBlur();
             }
             return true;
         }

src/com/android/launcher3/settings/SettingsMisc.java
@@ -36,6 +36,8 @@ import androidx.preference.PreferenceGroup.PreferencePositionCallback;
 import androidx.preference.PreferenceScreen;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.internal.util.bootleggers.BootlegUtils;
+
 import com.android.launcher3.LauncherAppState;
 import com.android.launcher3.LauncherFiles;
 import com.android.launcher3.R;
@@ -181,6 +183,11 @@ public class SettingsMisc extends FragmentActivity
                     // Show if plugins are enabled or flag UI is enabled.
                     return FeatureFlags.showFlagTogglerUi(getContext()) ||
                             PluginManagerWrapper.hasPlugins(getContext());
+
+                case Utilities.KEY_ALLOW_OVERVIEW_BLUR:
+                case Utilities.KEY_OVERVIEW_BLUR:
+                    return BootlegUtils.supportsBlur();
+
             }
             return true;
         }

Zeus-OS/android_packages_apps_ZeusLauncher

src/com/android/launcher3/settings/SettingsAppDrawer.java
@@ -39,6 +39,8 @@ import androidx.preference.PreferenceGroup.PreferencePositionCallback;
 import androidx.preference.PreferenceScreen;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.internal.util.palladium.PalladiumUtils;
+
 import com.android.launcher3.LauncherAppState;
 import com.android.launcher3.LauncherFiles;
 import com.android.launcher3.R;
@@ -180,6 +182,9 @@ public class SettingsAppDrawer extends FragmentActivity
                 case SUGGESTIONS_KEY:
                     // Show if Device Personalization Services is present.
                     return isDPSEnabled(getContext());
+                case Utilities.KEY_ALLOW_ALL_APPS_BLUR:
+                case Utilities.KEY_ALL_APPS_BLUR:
+                    return PalladiumUtils.supportsBlur();
             }
             return true;
         }

src/com/android/launcher3/settings/SettingsMisc.java
@@ -36,6 +36,8 @@ import androidx.preference.PreferenceGroup.PreferencePositionCallback;
 import androidx.preference.PreferenceScreen;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.internal.util.palladium.PalladiumUtils;
+
 import com.android.launcher3.LauncherAppState;
 import com.android.launcher3.LauncherFiles;
 import com.android.launcher3.R;
@@ -181,6 +183,11 @@ public class SettingsMisc extends FragmentActivity
                     // Show if plugins are enabled or flag UI is enabled.
                     return FeatureFlags.showFlagTogglerUi(getContext()) ||
                             PluginManagerWrapper.hasPlugins(getContext());
+
+                case Utilities.KEY_ALLOW_OVERVIEW_BLUR:
+                case Utilities.KEY_OVERVIEW_BLUR:
+                    return PalladiumUtils.supportsBlur();
+
             }
             return true;
         }